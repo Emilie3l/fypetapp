0347ca7fd8de2d418fafca41574ef23ff91f3767
Luis Zubia <soyzubia@gmail.com>
2020-11-24 14:06:08 -0700

    Fixed Added routes by Rails on routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,4 @@
 Rails.application.routes.draw do
-  get 'reports/index'
-  get 'reports/create'
-  get 'reports/new'
-  get 'reports/show'
-  get 'reports/edit'
-  get 'reports/update'
-  get 'reports/destroy'
-  get 'pets/create'
-  get 'pets/new'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
