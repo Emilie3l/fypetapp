f52740ba19634260ccdf8259b7159d036d203e62
Emilie Lockey <emilie.lockey@gmail.com>
2020-11-25 23:38:54 +0000

    seeds fixed

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -5,49 +5,46 @@ import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
 
+  const addMarkersToMap = (map, markers) => {
+    markers.forEach((marker) => {
+      const popup = new mapboxgl.Popup().setHTML(marker.infoWindow); // add this
+
+      const element = document.createElement('div');
+      element.className = 'marker';
+      element.style.backgroundImage = `url('${marker.image_url}')`;
+      element.style.backgroundSize = 'contain';
+      element.style.width = '25px';
+      element.style.height = '25px';
+
+      new mapboxgl.Marker(element)
+        .setLngLat([ marker.lng, marker.lat ])
+        .setPopup(popup)
+        .addTo(map);
+    });
+  };
+
   const fitMapToMarkers = (map, markers) => {
     const bounds = new mapboxgl.LngLatBounds();
     markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
     map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
-    };
-
-    if (mapElement) {
-      mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
-      const map = new mapboxgl.Map({
-        container: 'map',
-        style: 'mapbox://styles/mapbox/streets-v11'
-      });
-
-      const markers = JSON.parse(mapElement.dataset.markers);
-      markers.forEach((marker) => {
-        new mapboxgl.Marker()
-          .setLngLat([ marker.lng, marker.lat ])
-          .addTo(map);
-      });
-
-      map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
-                                      mapboxgl: mapboxgl }));
-
-      fitMapToMarkers(map, markers);
-    }
   };
 
-  const addMarkersToMap = (map, markers) => {
-  markers.forEach((marker) => {
-    const popup = new mapboxgl.Popup().setHTML(marker.infoWindow); // add this
+  if (mapElement) {
+    mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
+    const map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v11'
+    });
 
-    const element = document.createElement('div');
-    element.className = 'marker';
-    element.style.backgroundImage = `url('${marker.image_url}')`;
-    element.style.backgroundSize = 'contain';
-    element.style.width = '25px';
-    element.style.height = '25px';
+    const markers = JSON.parse(mapElement.dataset.markers);
 
-    new mapboxgl.Marker(element)
-      .setLngLat([ marker.lng, marker.lat ])
-      .setPopup(popup)
-      .addTo(map);
-  });
+    map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
+                                    mapboxgl: mapboxgl }));
+
+    addMarkersToMap(map, markers);
+
+    fitMapToMarkers(map, markers);
+    }
 };
 
 export { initMapbox };
