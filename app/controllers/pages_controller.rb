405ecaef9f3b19d58873ae8cadf0839a9b460717
Luis Zubia <soyzubia@gmail.com>
2020-11-27 12:33:07 -0700

    Fixed links at homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @recent_reunited = Report.where("status = :status", status: 'Reunited').order('created_at DESC').limit(3)
   end
 end
