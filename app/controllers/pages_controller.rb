e2dcd1ee1f06e41f8988f6ae5b8a9d9adc7a7780
Emilie Lockey <emilie.lockey@gmail.com>
2020-12-02 21:23:56 +0000

    Changed photos

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,8 +3,8 @@ class PagesController < ApplicationController
 
   def home
     @recent_reunited = Report.where(reunited: true).order('created_at DESC').limit(30).sample(3)
-    @testimonials = ["'So happy to be reunited with my best friend!!!'", "'I’m so relieved I found my little baby safe and sound. Couldn’t have done it without FYP!'", "'Finally an awesome app to find you lost pet!'"]
-    @user_photos = ["https://api.time.com/wp-content/uploads/2017/12/terry-crews-person-of-year-2017-time-magazine-2.jpg", "https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80", "https://images.unsplash.com/photo-1495366691023-cc4eadcc2d7e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80"]
+    @testimonials = ["'So happy to be reunited with my best friend!!! Thank you!'", "'I’m so relieved I found my little baby safe and sound. Couldn’t have done it without FYP!'", "'Finally an awesome app to find you lost pet! Highly recommend'"]
+    @user_photos = ["https://images.unsplash.com/photo-1546960977-3cfb9e3325c8?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80", "https://images.unsplash.com/photo-1493863641943-9b68992a8d07?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1639&q=80", "https://images.unsplash.com/photo-1495366691023-cc4eadcc2d7e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80"]
   end
 
   def myreports
